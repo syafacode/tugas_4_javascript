385bf2a00e98f7ea494fabb236a745063ffb4f7b
Syafa Athaya Firstnora <syafacode.01@gmail.com>
2020-04-28 00:30:39 +0700

    nested if

---
+++ tugas4.js
@@ -0,0 +1,21 @@
+function tinggiBadan() {
+    var adam = 175;
+    var khorim = 168;
+    var hawa = 160;
+
+    if (adam > khorim) {
+        if (adam > khorim) {
+            console.log("Tinggi Adam " + adam + "cm");
+        } else {
+            console.log("Adam kalah tinggi dengan Khorim")
+        }
+    } else {
+        if (adam > hawa) {
+            console.log("Tinggi Adam " + adam + "cm");
+        } else {
+            console.log("Adam kalah tinggi dengan Hawa")
+        }
+    }
+}
+
+tinggiBadan();
